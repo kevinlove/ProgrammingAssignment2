ba13e2ae22f018c2ad4636757a49cb18f56a8852
Kevin Love <klove@flmnh.ufl.edu>
2014-08-20 16:37:14 -0400

    First commit of assignment files

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions @makeCacheMatrix and @cacheSolve represent programming assignment #2 for rprog-006
+## Each of these functions cache potentially time-consuming computations
+## 
 
-## Write a short comment describing this function
+## @makeCacheMatrix This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setMatrix <- function(solve) m <<- solve
+        getMatrix <- function() m
+        list(set = set, get = get,
+             setMatrix = setMatrix,
+             getMatrix = getMatrix)        
 }
 
-
-## Write a short comment describing this function
+## @cacheSolve This function computes the inverse of the special "matrix" returned by @makeCacheMatrix 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getMatrix()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setMatrix(m)
+        m
 }
